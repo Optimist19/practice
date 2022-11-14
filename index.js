c39e864ffd2a2e0ec98a08314eff55ad7cce109c
optimist19 <oluwasegunbamidele3@gmail.com>
2022-11-14 15:33:45 +0100

    It's actaually a count in pure javascript, just practising

---
+++ index.js
@@ -0,0 +1,43 @@
+					// For Home count
+	
+	let home = document.getElementById("home_digits")
+	
+	let number = 0
+	function add1(){
+		number += 1;
+		home.innerHTML = number 
+	}
+	
+	function add2(){
+		number += 2
+		home.innerHTML = number 
+	}
+	
+	function add3(){
+		number += 3
+		home.innerHTML = number 
+	}
+	
+	// For Home count
+	
+let guest = document.getElementById("guest_digits")
+
+let g_number = 0;
+
+function g_add1(){
+	g_number = g_number + 1
+	guest.innerHTML = g_number
+}
+
+function g_add2(){
+	g_number = g_number + 2
+	guest.innerHTML = g_number
+}
+
+
+function g_add3(){
+	g_number = g_number + 3
+	guest.innerHTML = g_number
+}
+
+
